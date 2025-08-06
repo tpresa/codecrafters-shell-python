97fc4dd253b4d6c2432a29e3e855a3f8a9c3b61c
Thiago Presa <thiago.addevico@gmail.com>
2025-08-06 15:21:00 -0300

    Implement invalid command

--- app/main.py
+++ app/main.py
@@ -6,8 +6,9 @@ def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
+    command = input()
 
+    print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
